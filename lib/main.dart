d0fde4b43bcf1d520d6b986addb069a1c24b413a
Ramez Nasser <rameznasser55@gmail.com>
2024-06-30 15:43:30 +0300

    Add Firebase configuration for FlutterFire integration
    - Applied Google Services plugin in build.gradle for Firebase support.
    - Added google-services.json for Firebase project configuration.
    - Updated settings.gradle to include Google Services plugin.
    - Created firebase.json for FlutterFire CLI configuration.
    - Generated firebase_options.dart for platform-specific Firebase options.
    - Modified main.dart to initialize Firebase with platform-specific options.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:auth_app/features/auth_feature/logic/auth_bloc/auth_bloc.dart';
 import 'package:auth_app/features/auth_feature/presentation/views/login_page.dart';
+import 'package:auth_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,7 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const AuthApp());
 }
